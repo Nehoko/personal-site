ad42b0f65485c564297680b390d0adc7a73cded7
Ilya Mikhailov <fun4lsd@gmail.com>
2022-08-24 19:54:07 +0400

    favicon added.

--- src/pages/TechStackPage.tsx
+++ src/pages/TechStackPage.tsx
@@ -1,5 +1,28 @@
 import React from 'react'
 
 export function TechStackPage() {
-    return <div>Tech Stack Page</div>
+    return (
+        <div>
+            <h3>Tech Stack:</h3>
+            <br />
+            <h4>Backend:</h4>
+            <br />
+            <h5>Languages:</h5>
+            <br />
+            <ul className="list-disc">
+                <li>java 8-17</li>
+                <li>kotlin</li>
+            </ul>
+            <br />
+            <h5>Frameworks:</h5>
+            <br />
+            <ul className="list-disc">
+                <li>Spring 5</li>
+                <li>Spring boot</li>
+                <li>Spring MVC</li>
+                <li>Spring Cloud</li>
+                <li>Spring Security</li>
+            </ul>
+        </div>
+    )
 }
